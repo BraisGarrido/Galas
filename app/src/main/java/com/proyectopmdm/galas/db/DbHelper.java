80f09bbac30529acacfe214ce0edfcca87845eaf
BraisGarrido <braisgb@gmail.com>
2023-03-08 18:24:02 +0100

    Mejora entradas en la base de datos

--- app/src/main/java/com/proyectopmdm/galas/db/DbHelper.java
+++ app/src/main/java/com/proyectopmdm/galas/db/DbHelper.java
@@ -17,29 +17,29 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE "+TABLE_GALAS+"(id INTEGER PRIMARY KEY AUTOINCREMENT, year TEXT NOT NULL, film TEXT, director TEXT)");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2000', 'American beauty', 'Sam Mendes')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2001', 'Gladiator', 'Steven Soderbergh')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2002', 'A beautiful mind', 'Ron Howard')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2003', 'Chicago', 'Roman Polanski')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2004', 'El señor de los anillos: El retorno del rey', 'PeterJackson')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2005', 'Million dollar baby', 'Clim Eastwood')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2006', 'Crash', 'Ang Lee')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2007', 'The departed', 'Martin Scorsese')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2008', 'No country for old men', 'Joel Coen & Ethan Coen')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2009', 'Slumdog millionaire', 'Dany Boyle')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2010', 'The hurt locker', 'Kathryn Bigelow')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2011', 'El discurso del rey', 'Tom Hooper')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2012', 'The artist', 'Michel Hazanavicius')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2013', 'Argo', 'Ang Lee')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2014', '12 años de esclavitud', 'Alfonso Cuarón')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2015', 'Birdman', 'Alejandro González Iñárritu')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2016', 'Spotlight', 'Alejandro González Iñárritu')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2017', 'Moonlight', 'Damien Chazelle')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2018', 'La forma del agua', 'Guillermo del Toro')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2019', 'Green Book', 'Alfonso Cuarón')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2020', 'Parásitos', 'Bong Joon-ho')");
-        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2021', 'Nomadland', 'Chloé Zhao')");
         db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2022', 'CODA', 'Jane Campion')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2021', 'Nomadland', 'Chloé Zhao')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2020', 'Parásitos', 'Bong Joon-ho')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2019', 'Green Book', 'Alfonso Cuarón')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2018', 'La forma del agua', 'Guillermo del Toro')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2017', 'Moonlight', 'Damien Chazelle')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2016', 'Spotlight', 'Alejandro González Iñárritu')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2015', 'Birdman', 'Alejandro González Iñárritu')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2014', '12 años de esclavitud', 'Alfonso Cuarón')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2013', 'Argo', 'Ang Lee')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2012', 'The artist', 'Michel Hazanavicius')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2011', 'El discurso del rey', 'Tom Hooper')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2010', 'The hurt locker', 'Kathryn Bigelow')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2009', 'Slumdog millionaire', 'Dany Boyle')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2008', 'No country for old men', 'Joel Coen & Ethan Coen')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2007', 'The departed', 'Martin Scorsese')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2006', 'Crash', 'Ang Lee')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2005', 'Million dollar baby', 'Clim Eastwood')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2004', 'El señor de los anillos: El retorno del rey', 'PeterJackson')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2003', 'Chicago', 'Roman Polanski')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2002', 'A beautiful mind', 'Ron Howard')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2001', 'Gladiator', 'Steven Soderbergh')");
+        db.execSQL("INSERT INTO "+TABLE_GALAS+" (year, film, director) VALUES ('2000', 'American beauty', 'Sam Mendes')");
     }
 
     @Override
